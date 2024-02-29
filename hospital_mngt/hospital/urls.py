4d9dc1f55697ce1b4ee7b1f552006d5ebc1fea3e
Ayush-Dutta <100012679+Ayush-Dutta@users.noreply.github.com>
2024-03-01 00:56:35 +0530

    Third Commit

--- hospital_mngt/hospital/urls.py
+++ hospital_mngt/hospital/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import About, Home
-
+from .views import About, Home, Contact
 urlpatterns = [
     path('', Home, name='home'),
     path('about/', About, name='about'),
+    path('contact/', Contact, name='contact'),
 ]
