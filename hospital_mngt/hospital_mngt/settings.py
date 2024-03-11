42f773bee88cafb0fdbb5c36b0621ddc8b04f442
Ayush-Dutta <100012679+Ayush-Dutta@users.noreply.github.com>
2024-03-11 10:54:44 +0530

    eighth commit

--- hospital_mngt/hospital_mngt/settings.py
+++ hospital_mngt/hospital_mngt/settings.py
@@ -116,8 +116,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [ BASE_DIR , 'static']
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static',  # Add 'static' directory in your project directory
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
